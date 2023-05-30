729beea2b7f91a039cc63371761c8201ef7e0b91
GobiHobi <gobihobi1@gmail.com>
2023-05-30 22:48:30 +0000

    brnach2

--- tools/iget.sh
+++ tools/iget.sh
@@ -1,52 +1,28 @@
-# This gets all the files in a current directory
-# files=( * )
-
-
 #!/bin/bash
 
 # Check if there are any command line arguments
 if [ $# -eq 0 ]; then
-  
   # No command line arguments were given, so ask for one
   echo "Please enter a command line argument:"
-  read $pp
-
+  read pp
 else
   # pipeline parameter
   pp=$1
-  
 fi
 
 # regex expression that checks if a string - or whatever type - is numbers, weird
 re='^[0-9]+$'
-
 if [[ $pp =~ $re ]] ; then
-
   #  Get the current directory
   current_dir=$(pwd)
-  
-  # Get the list of files in the current directory
+  # Get the list of file paths in the directory it was ran on
   files=( "$current_dir"/* )
-
+  # Get the file path through index position
   file=${files[$pp]}
-
-  if [[ $pp -le 0 ]] && [[ $pp -ge ${#array[@]} ]]; then
-  
-    return $file
-    
-  else
-  
-    echo "Error, not a valid index position"
-    
-    exit
-    
-  fi
-
+  # Return filename path
+  echo $file
   # error, if $pp isn't an integer
   else
-
-    echo "Please input an integer"
-
+    echo "Error, please input an integer"
     exit
-    
 fi
--- tools/init.sh
+++ tools/init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "The links"
+else
+  git init
+fi 
 docs="https://docs.google.com/document/d/1y6hnrlTUuzqfdGC-XWjXSNaBXLxc1fv00BvBaC5o87M/edit"
 github="https://github.com/GobiHobi/linux-config"
 bard="https://bard.google.com/"
